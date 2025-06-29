f39b064d61e5f09a63ea7f79e3c45af3e2f30f68
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-06-29 13:03:49 -0300

    feat: update Home component with HomeHeader and mini logo, adjust Navigator height, and create Activity module with controller, service, and module files

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,13 +1,15 @@
 import Navigator from "@/src/components/navegator";
 import { NavigationProvider } from "@/src/contexts/NavigationContext";
-import { View,Text } from "react-native";
+import { View,Text,Image } from "react-native";
+import miniLogo from '../../../images/logo-mini.png'
+import HomeHeader from "@/src/components/HomeHeader";
 
 export default function Home (){
   return(
     <NavigationProvider>
-        <View className="h-[850px] w-screen bg-white">
-          <Navigator />
-         </View>
+         <HomeHeader />
+          
+          <Navigator /> 
     </NavigationProvider>
    
   )
---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Image } from 'react-native';
+import miniLogo from '../../images/logo-mini.png'; 
+
+export default function HomeHeader() {
+  return (
+    <View className="w-full bg-white p-8 mb-4">
+      <View className="flex-col">
+        <View className="flex-row items-center space-x-2">
+          <Image source={miniLogo} className="w-[60px] h-[60px] mr-10" />
+          <Text className="text-2xl font-bold">Início</Text>
+        </View>
+        <Text className="text-xl font-semibold mt-2">Suas atividades</Text>
+      </View>
+    </View>
+  );
+}
